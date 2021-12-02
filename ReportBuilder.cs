2c857e273df7ed851d34092d679097d8def5c196
shivanikaktikar <shivanikaktikar@gmail.com>
2021-12-02 10:38:32 +0530

    This is abstract class

---
+++ ReportBuilder.cs
@@ -0,0 +1,25 @@
+//This is going to be an abstract class and this class provides the blueprint to create different types of beverages.
+
+using System;
+namespace BuilderDesignPattern
+{
+    public abstract class ReportBuilder
+    {
+        protected Report reportObject;
+
+        public abstract void SetReportType();
+        public abstract void SetReportHeader();
+        public abstract void SetReportContent();
+        public abstract void SetReportFooter();
+
+        public void CreateNewReport()
+        {
+            reportObject = new Report();
+        }
+
+        public Report GetReport()
+        {
+            return reportObject;
+        }
+    }
+}
