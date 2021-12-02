bd4e4ec43856b47e1f6b60bdf74c9bb19e217c7a
shivanikaktikar <shivanikaktikar@gmail.com>
2021-12-02 10:42:47 +0530

    Contains Main method

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace BuilderDesignPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Client Code
+            Report report;
+            ReportDirector reportDirector = new ReportDirector();
+
+            // Construct and display Reports
+            PDFReport pdfReport = new PDFReport();
+            report = reportDirector.MakeReport(pdfReport);
+            report.DisplayReport();
+
+            Console.WriteLine("-------------------");
+
+            ExcelReport excelReport = new ExcelReport();
+            report = reportDirector.MakeReport(excelReport);
+            report.DisplayReport();
+            
+            Console.ReadKey();
+        }
+    }
+}
